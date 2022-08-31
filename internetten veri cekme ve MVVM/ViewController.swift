d1aa3de8a443e22acfe42b7294b6c8d32a5bdd95
mert <mert.ma346@gmail.com>
2022-08-31 19:44:05 +0300

    Initial Commit

---
+++ internetten veri cekme ve MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  internetten veri cekme ve MVVM
+//
+//  Created by Mert AKBAŞ on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
