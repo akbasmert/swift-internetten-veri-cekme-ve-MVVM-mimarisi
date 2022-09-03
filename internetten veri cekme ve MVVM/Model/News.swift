c88358aa9a74879455a3f01e3849142f8b0796e2
mert <mert.ma346@gmail.com>
2022-09-03 15:24:29 +0300

    escaping işlemi yaptık

---
+++ internetten veri cekme ve MVVM/Model/News.swift
@@ -0,0 +1,14 @@
+//
+//  News.swift
+//  internetten veri cekme ve MVVM
+//
+//  Created by Mert AKBAŞ on 3.09.2022.
+//
+
+import Foundation
+
+struct News: Decodable {
+    let title: String
+    let stroy:String
+    
+}
---
+++ internetten veri cekme ve MVVM/Service/Webservice.swift
@@ -0,0 +1,15 @@
+//
+//  Webservice.swift
+//  internetten veri cekme ve MVVM
+//
+//  Created by Mert AKBAŞ on 3.09.2022.
+//
+
+import Foundation
+
+class Webservice {
+    
+    func HaberleriIndir(url: URL, Completion: @escaping([News]?)->()){ //escaping yaptık çünkü tamamlanması zaman alan işlemler. News i de ekledik.
+        
+    }
+}
